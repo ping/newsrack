44b52781cf69257013055d1767e51456c9fada1c
ping <ping@users.noreply.github.com>
2022-04-17 19:13:14 +0800

    Minor non-recipe fixes [skip ci]

--- _build.sh
+++ _build.sh
@@ -1,7 +1,7 @@
 sh _prebuild.sh
 mkdir -p public \
 && cp -p static/favicon.svg public/ \
-&& sassc -t compressed static/site.scss static/site.css || sass -s compressed --no-source-map static/site.scss static/site.css \
+&& sass -s compressed --no-source-map static/site.scss static/site.css \
 && python3 _generate.py \
 && html-minifier-terser --input-dir public/ --output-dir public/ --minify-js --collapse-whitespace --file-ext html \
 && rm *.recipe
