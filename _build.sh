3b24180bee39cd5bd966f83b765f0e6b6e9fe8d6
ping <ping@users.noreply.github.com>
2022-04-22 12:16:25 +0800

    Test env

--- _build.sh
+++ _build.sh
@@ -3,9 +3,11 @@ for f in *.recipe.py; do
     cp -p "$f" "${f%.py}"
 done
 
+echo "$CI_PAGES_URL"
+
 mkdir -p public \
 && cp -p static/favicon.svg public/ \
 && sass -s compressed --no-source-map static/site.scss static/site.css \
-&& python3 _generate.py "$CI_PAGES_URL" \
+&& python3 _generate.py "https://ping.github.io/newsrack/" \
 && html-minifier-terser --input-dir public/ --output-dir public/ --minify-js --collapse-whitespace --file-ext html \
 && rm *.recipe
