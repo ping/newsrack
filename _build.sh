fe1abe5e086a49a9fa32c797cd154cc20ac0af38
ping <ping@users.noreply.github.com>
2022-05-24 11:31:12 +0800

    chore: Make rm a little safer

--- _build.sh
+++ _build.sh
@@ -8,4 +8,4 @@ mkdir -p public cache \
 && sass -s compressed --no-source-map static/site.scss static/site.css \
 && python3 _generate.py "$CI_PAGES_URL" \
 && html-minifier-terser --input-dir public/ --output-dir public/ --minify-js --collapse-whitespace --file-ext html \
-&& rm *.recipe
+&& rm -f *.recipe
