de5bcab8ab3eb5fed2d3858b4c963daa34422bc2
ping <ping@users.noreply.github.com>
2022-06-06 09:26:33 +0800

    feat: Minor UI tweaks

--- _build.sh
+++ _build.sh
@@ -4,7 +4,7 @@ for f in *.recipe.py; do
 done
 
 mkdir -p public cache \
-&& cp -p static/favicon.svg public/ \
+&& cp -p static/*.svg public/ \
 && sass -s compressed --no-source-map static/site.scss static/site.css \
 && python3 _generate.py "$CI_PAGES_URL" \
 && html-minifier-terser --input-dir public/ --output-dir public/ --minify-js --collapse-whitespace --file-ext html \
