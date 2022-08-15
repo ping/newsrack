71ab90ec9adebf33ecf887feb3442d02429a6203
ping <ping@users.noreply.github.com>
2022-08-16 07:14:39 +0800

    Increase timeout for Asahi Shimbun [skip ci]

--- recipes/asahi-shimbun.recipe.py
+++ recipes/asahi-shimbun.recipe.py
@@ -44,7 +44,7 @@ class AsahiShimbunEnglishNews(BasicNewsRecipe):
     compress_news_images = True
     scale_news_images = (800, 800)
     scale_news_images_to_device = False  # force img to be resized to scale_news_images
-    timeout = 20
+    timeout = 40
     timefmt = ""
     pub_date = None  # custom publication date
 
