abfcf5b6bd9c10c66512d8ed1314e4e179eec72b
ping <ping@users.noreply.github.com>
2022-11-17 07:29:09 +0800

    NYT: Increase recipe timeout [skip ci]

--- _recipes.py
+++ _recipes.py
@@ -364,7 +364,7 @@ recipes: List[Recipe] = [
         src_ext="mobi",
         target_ext=["epub"],
         category="News",
-        timeout=600,
+        timeout=900,
         retry_attempts=0,
         enable_on=onlyat_hours(
             list(range(0, 4)) + list(range(8, 18)) + list(range(22, 24))
