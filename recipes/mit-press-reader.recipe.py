1a9ec9acddbad5d9bd3255d8a3d0aa76195d7b6d
ping <ping@users.noreply.github.com>
2022-12-16 13:35:51 +0800

    Fix MIT Press Reader since the WP endpoints are now restricted

--- recipes/mit-press-reader.recipe.py
+++ recipes/mit-press-reader.recipe.py
@@ -3,15 +3,7 @@
 # This software is released under the GNU General Public License v3.0
 # https://opensource.org/licenses/GPL-3.0
 
-import json
-import shutil
-import time
-from datetime import datetime, timedelta, timezone
-from html import unescape
-from urllib.parse import urlencode
-
-from calibre.ebooks.BeautifulSoup import BeautifulSoup
-from calibre.ptempfile import PersistentTemporaryDirectory, PersistentTemporaryFile
+from calibre.utils.date import parse_date
 from calibre.web.feeds.news import BasicNewsRecipe
 
 _name = "The MIT Press Reader"
@@ -39,15 +31,26 @@ class MITPressReader(BasicNewsRecipe):
     pub_date = None  # custom publication date
     temp_dir = None
 
+    keep_only_tags = [dict(class_=["article-entry"])]
     remove_tags = [
         dict(name=["script", "noscript", "style"]),
+        dict(
+            class_=[
+                "ma-top-shares-right",
+                "ma-txt-customizer-cont",
+                "social-cont",
+                "ma-related-posts",
+                "tags-cont",
+            ]
+        ),
     ]
 
     extra_css = """
-    .headline { font-size: 1.8rem; margin-bottom: 0.4rem; }
-    .article-meta { padding-bottom: 0.5rem; }
-    .article-meta .author { font-weight: bold; color: #444; margin-right: 0.5rem; }
+    h1 { font-size: 1.8rem; margin-bottom: 0.4rem; }
+    .ma-subheading { font-size: 1.2rem; font-style: italic; margin-bottom: 1rem; }
+    .ma-authors { font-weight: bold; color: #444; margin-right: 0.5rem; }
     .article-section { display: block; font-weight: bold; color: #444; }
+    .wp-block-image { margin-bottom: 0.5rem; }
     .wp-block-image img {
         display: block; margin-bottom: 0.3rem; max-width: 100%; height: auto;
         box-sizing: border-box;
@@ -58,188 +61,24 @@ class MITPressReader(BasicNewsRecipe):
     """
 
     feeds = [
-        (_name, "https://thereader.mitpress.mit.edu/wp-json/wp/v2/posts"),
+        (_name, "https://thereader.mitpress.mit.edu/feed/"),
     ]
 
-    def _extract_featured_media(self, post, soup):
-        """
-        Include featured media with post content.
-
-        :param post: post dict
-        :param post_content: Extracted post content
-        :return:
-        """
-        post_soup = BeautifulSoup(post["content"]["rendered"])
-        for img in post_soup.find_all("img", attrs={"data-src": True}):
-            img["src"] = img["data-src"]
-        post_content = str(post_soup)
-        if not post.get("featured_media"):
-            return post_content
-
-        feature_media_css = f"wp-image-{post['featured_media']}"
-        if feature_media_css in post_content:
-            # check already not embedded
-            return post_content
-
-        for feature_info in post.get("_embedded", {}).get("wp:featuredmedia", []):
-            # put feature media at the start of the post
-            if feature_info.get("source_url"):
-                caption = feature_info.get("caption", {}).get("rendered", "")
-                # higher-res
-                image_src = f"""
-                <div class="wp-block-image">
-                    <img src="{feature_info["source_url"]}">
-                    <div class="image-credit">{caption}</div>
-                </div>"""
-                post_content = image_src + post_content
-            else:
-                post_content = (
-                    feature_info.get("description", {}).get("rendered", "")
-                    + post_content
-                )
-        return post_content
-
-    def preprocess_raw_html(self, raw_html, url):
-        # formulate the api response into html
-        post = json.loads(raw_html)
-        date_published_loc = datetime.strptime(post["date"], "%Y-%m-%dT%H:%M:%S")
-        soup = BeautifulSoup(
-            f"""<html>
-        <head><title></title></head>
-        <body>
-            <article data-og-link="{post["link"]}">
-            <h1 class="headline"></h1>
-            <div class="article-meta">
-                <span class="published-dt">
-                    {date_published_loc:%-I:%M%p, %-d %b, %Y}
-                </span>
-            </div>
-            </article>
-        </body></html>"""
-        )
-        soup.head.title.string = unescape(post["title"]["rendered"])
-        soup.find("h1").string = unescape(post["title"]["rendered"])
-        soup.body.article.append(
-            BeautifulSoup(self._extract_featured_media(post, soup))
-        )
-        # inject authors
-        try:
-            post_authors = [
-                a["name"] for a in post.get("_embedded", {}).get("author", [])
-            ]
-            if post_authors:
-                soup.find(class_="article-meta").insert(
-                    0,
-                    BeautifulSoup(
-                        f'<span class="author">{", ".join(post_authors)}</span>'
-                    ),
-                )
-        except (KeyError, TypeError):
-            pass
-        # inject categories
-        if post.get("categories"):
-            categories = []
-            try:
-                for terms in post.get("_embedded", {}).get("wp:term", []):
-                    categories.extend(
-                        [t["name"] for t in terms if t["taxonomy"] == "category"]
-                    )
-            except (KeyError, TypeError):
-                pass
-            if categories:
-                soup.body.article.insert(
-                    0,
-                    BeautifulSoup(
-                        f'<span class="article-section">{" / ".join(categories)}</span>'
-                    ),
-                )
-        return str(soup)
-
-    def populate_article_metadata(self, article, soup, first):
-        # pick up the og link from preprocess_raw_html() and set it as url instead of the api endpoint
-        og_link = soup.select("[data-og-link]")
-        if og_link:
-            article.url = og_link[0]["data-og-link"]
+    def postprocess_html(self, soup, _):
+        time_ele = soup.find(name="time", attrs={"datetime": True})
+        post_date = parse_date(time_ele["datetime"])
+        if (not self.pub_date) or post_date > self.pub_date:
+            self.pub_date = post_date
+            self.title = f"{_name}: {post_date:%-d %b, %Y}"
+        athor_ele = soup.find(class_="ma-top-shares-left")
+        if athor_ele:
+            post_date_ele = soup.new_tag("div")
+            post_date_ele.append(f"{post_date:%-H:%M%p, %-d %B, %Y}")
+            athor_ele.append(post_date_ele)
+            time_parent_ele = soup.find("div", class_="author-post-cont")
+            if time_parent_ele:
+                time_parent_ele.decompose()
+        return soup
 
     def publication_date(self):
         return self.pub_date
-
-    def cleanup(self):
-        if self.temp_dir:
-            self.log("Deleting temp files...")
-            shutil.rmtree(self.temp_dir)
-
-    def parse_index(self):
-        br = self.get_browser()
-        per_page = 100
-        articles = {}
-        self.temp_dir = PersistentTemporaryDirectory()
-
-        for feed_name, feed_url in self.feeds:
-            posts = []
-            page = 1
-            while True:
-                cutoff_date = datetime.today().replace(
-                    hour=0, minute=0, second=0, microsecond=0
-                ) - timedelta(days=self.oldest_article)
-
-                params = {
-                    "page": page,
-                    "per_page": per_page,
-                    "after": cutoff_date.isoformat(),
-                    "_embed": "1",
-                    "_": int(time.time() * 1000),
-                }
-                endpoint = f"{feed_url}?{urlencode(params)}"
-                try:
-                    res = br.open_novisit(endpoint)
-                    posts_json_raw = res.read().decode("utf-8")
-                    retrieved_posts = json.loads(posts_json_raw)
-                    if not retrieved_posts:
-                        break
-                    posts.extend(retrieved_posts)
-                    try:
-                        # abort early to save one extra request
-                        headers = res.info()
-                        if headers.get("x-wp-totalpages"):
-                            wp_totalpages = int(headers["x-wp-totalpages"])
-                            if wp_totalpages == page:
-                                break
-                    except:
-                        # do nothing else if we can't parse headers for page info
-                        # rely on HTTP 400 to detect paging break
-                        pass
-                    page += 1
-                except:  # HTTP 400
-                    break
-
-            latest_post_date = None
-            for p in posts:
-                post_update_dt = datetime.strptime(
-                    p["modified_gmt"], "%Y-%m-%dT%H:%M:%S"
-                ).replace(tzinfo=timezone.utc)
-                if not self.pub_date or post_update_dt > self.pub_date:
-                    self.pub_date = post_update_dt
-                post_date = datetime.strptime(p["date"], "%Y-%m-%dT%H:%M:%S")
-                if not latest_post_date or post_date > latest_post_date:
-                    latest_post_date = post_date
-                    self.title = f"{_name}: {post_date:%-d %b, %Y}"
-
-                section_name = f"{post_date:%-d %B, %Y}"
-                if len(self.get_feeds()) > 1:
-                    section_name = f"{feed_name}: {post_date:%-d %B, %Y}"
-                if section_name not in articles:
-                    articles[section_name] = []
-
-                with PersistentTemporaryFile(suffix=".json", dir=self.temp_dir) as f:
-                    f.write(json.dumps(p).encode("utf-8"))
-                articles[section_name].append(
-                    {
-                        "title": p["title"]["rendered"] or "Untitled",
-                        "url": "file://" + f.name,
-                        "date": f"{post_date:%-d %B, %Y}",
-                        "description": p["excerpt"]["rendered"],
-                    }
-                )
-
-        return articles.items()
