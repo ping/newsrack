59a4d5484c7e19a1f615ec7e020e344348e5ae10
ping <ping@users.noreply.github.com>
2023-01-12 15:46:39 +0800

    Fix debug.sh to generate the required env variable for recipes [skip ci]

--- debug.sh
+++ debug.sh
@@ -6,6 +6,13 @@ then
     exit 9
 fi
 
+get_abs_dirname() {
+  # $1 : relative filename
+  echo "$(cd "$(dirname "$1")" && pwd)/"
+}
+# use to get shared code
+export recipes_includes=$(get_abs_dirname "recipes/includes/recipes_shared.py")
+
 for recipe_folder in 'recipes' 'recipes_custom'
 do
     if [ -f "$recipe_folder/$1.recipe.py" ]; then
