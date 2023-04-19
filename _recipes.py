1ad02c15e75830f19558996ae41698446f95958b
ping <ping@users.noreply.github.com>
2023-04-19 23:38:00 +0800

    Tweak NYT timings [skip ci]

--- _recipes.py
+++ _recipes.py
@@ -406,7 +406,7 @@ recipes: List[Recipe] = [
         timeout=900,
         retry_attempts=0,
         enable_on=onlyat_hours(
-            list(range(0, 4)) + list(range(8, 18)) + list(range(22, 24))
+            list(range(0, 8)) + list(range(12, 18)) + list(range(22, 24))
         ),
         cover_options=CoverOptions(
             logo_path_or_url="https://static01.nyt.com/newsgraphics/2015/12/23/masthead-2016/8118277965bda8228105578895f2f4a7aeb22ce2/nyt-logo.png"
@@ -420,7 +420,7 @@ recipes: List[Recipe] = [
         category="News",
         timeout=1200,
         retry_attempts=0,
-        enable_on=onlyat_hours(list(range(4, 8))),
+        enable_on=onlyat_hours(list(range(8, 12))),
         cover_options=CoverOptions(
             logo_path_or_url="https://static01.nyt.com/newsgraphics/2015/12/23/masthead-2016/8118277965bda8228105578895f2f4a7aeb22ce2/nyt-logo.png"
         ),
